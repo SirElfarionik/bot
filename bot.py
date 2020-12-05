2f8a885d60dc1835fe8b0ace5ff9c32d29c0cef6
SirElfarionik <75225077+SirElfarionik@users.noreply.github.com>
2020-12-05 20:41:09 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ API_TOKEN = '1432648425:AAECn_Bt89nHzS_XFN2PFRGNYtHKGUQaL4g'
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
+lentayevo_id = '-1001486134019'
 
 btn_what_day_tomorrow = InlineKeyboardButton(text='На завтра?', callback_data='Tom')
 btn_what_day_not_tomorrow = InlineKeyboardButton(text='Другой день', callback_data='NotTom')
@@ -19,6 +20,7 @@ async def hw(message: types.Message):
     ids = message.chat.id
     await bot.send_message(message.chat.id, 'Выберете дату', reply_markup=markup)
     ids = message.chat.id
+
 @dp.message_handler(commands=['helps'])
 async def helps(message: types.Message):
     await bot.send_message(message.chat.id, 'Введите команду "/hw" или дату в формате dd.mm.yyyy')
@@ -32,7 +34,10 @@ async def tom_date(query: types.callback_query):
         text = get_hw.get_hw(get_hw.check_day())
     elif answer_data == 'NotTom':
         text = 'Введите дату в формате dd.mm.yyyy'
-    await bot.send_message(ids, text)
+    if query.from_user.id == lentayevo_id:
+        await bot.send_message(lentayevo_id, text)
+    else:
+        await bot.send_message(query.from_user.id, text)
 @dp.message_handler()
 async def check_message(message: types.Message):
     try:
